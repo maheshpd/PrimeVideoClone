92d3562be2a82a4c0679a78197f6a098613bdf0b
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-07-05 15:38:08 +0530

    Setup Model and Adapter Class

---
+++ app/src/main/java/com/createsapp/primevideoclone/model/BannerMovies.java
@@ -0,0 +1,47 @@
+package com.createsapp.primevideoclone.model;
+
+public class BannerMovies {
+    Integer id;
+    String moviesName;
+    String imageUrl;
+    String fileUrl;
+
+    public BannerMovies(Integer id, String moviesName, String imageUrl, String fileUrl) {
+        this.id = id;
+        this.moviesName = moviesName;
+        this.imageUrl = imageUrl;
+        this.fileUrl = fileUrl;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMoviesName() {
+        return moviesName;
+    }
+
+    public void setMoviesName(String moviesName) {
+        this.moviesName = moviesName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+}
